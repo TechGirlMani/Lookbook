47f63a34449e87c7b53b8eb6f39e71771936b2b4
TechGirlMani <techgirlmani@gmail.com>
2024-03-22 21:56:07 -0400

    added middleware to be able to move to next middleware in server.js file

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,16 @@
+require('dotenv').config()
+
 const express = require('express')
 
 // create express app
 const app = express()
+
+// middleware 
+// to be able to move to next middleware
+app.use((req, rest, next) => {
+    console.log(req.path, req.method)
+    next()
+})
 
 // routes
 app.get('/', (req, res) => {
@@ -9,6 +18,6 @@ app.get('/', (req, res) => {
 })
 
 // listen to requests on port 4000
-app.listen(4000, () => {
-    console.log('listening on port 4000')
+app.listen(process.env.PORT, () => {
+    console.log('listening on port', process.env.PORT)
 })
