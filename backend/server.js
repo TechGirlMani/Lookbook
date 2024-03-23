5a5e9286c3f0950814f67173f034b3858ca866d1
TechGirlMani <techgirlmani@gmail.com>
2024-03-22 21:46:34 -0400

    created a route in server.js file and created a .env file for sensitive info

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,11 @@ const express = require('express')
 // create express app
 const app = express()
 
+// routes
+app.get('/', (req, res) => {
+    res.json({mssg: 'Welcome to the app'})
+})
+
 // listen to requests on port 4000
 app.listen(4000, () => {
     console.log('listening on port 4000')
