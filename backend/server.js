30d26f3d2bacfe8a8b23cc075eddcc78edc4c7c0
TechGirlMani <techgirlmani@gmail.com>
2024-03-22 22:31:34 -0400

    added my connection string to connect to my db)

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const lookbookRoutes = require('./routes/lookbooks')
 
 // create express app
@@ -18,7 +19,17 @@ app.use((req, rest, next) => {
 app.use('/api/lookbooks', lookbookRoutes)
 
 
-// listen to requests on port 4000
-app.listen(process.env.PORT, () => {
-    console.log('listening on port', process.env.PORT)
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    // listen to requests on port 4000
+    app.listen(process.env.PORT, () => {
+        console.log('connected to db & listening on port', process.env.PORT)
+    })
 })
+.catch((error) => {
+    console.error(error)
+})
+
+
+
